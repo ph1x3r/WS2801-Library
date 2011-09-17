ed3e71ab2a951f7d1bd74d3f5888c34312f24d21
ph1x3r <ph1x3r@gmail.com>
2011-09-16 23:22:50 -0400

    Fixed memory allocation bug. Added new background color functions. Tidy up of code and more comments inline.

--- WS2801.cpp
+++ WS2801.cpp
@@ -3,9 +3,9 @@
 #include <pins_arduino.h>
 #include <SPI.h>
 
-// Example to control WS2801-based RGB LED Modules in a strand or strip
-// Written by Adafruit - MIT license
-/*****************************************************************************
+/* Example to control WS2801-based RGB LED Modules in a strand or strip
+ * Written by Adafruit - MIT license
+ *****************************************************************************
  * Additions made by ph1x3r (ph1x3r@gmail.com) for manipulating the LED
  * pixels when they are organized as a grid
  *
@@ -21,48 +21,47 @@
 // used in the line drawing algorithm
 #define swap(a, b) { int16_t t = a; a = b; b = t; }
 
-// SPI ports are part of the hardware design. Use defaults.
-// pin names from the pins_arduino.h file
-// only DATAOUT and SPICLOCK are used, the rest are ignored
-// =====+=====+========
-// pin  | Uno | Teensy2
-// =====+=====+========
-// MOSI | 11  | 2
-// MISO | 12  | 3
-// SCLK | 13  | 1
-// SS   | 10  | 0
-
-/*
-Data for setting the SPI control register - informational only
-SPCR
-| 7    | 6    | 5    | 4    | 3    | 2    | 1    | 0    |
-| SPIE | SPE  | DORD | MSTR | CPOL | CPHA | SPR1 | SPR0 |
-
-SPIE - Enables the SPI interrupt when 1
-SPE  - Enables the SPI when 1
-DORD - Sends data least Significant Bit First when 1, most Significant Bit first
-MSTR - Sets the Arduino in master mode when 1, slave mode when 0
-CPOL - Sets the data clock to be idle when high if set to 1, idle when low if se
-CPHA - Samples data on the falling edge of the data clock when 1, rising edge wh
-SPR1 and SPR0 - Sets the SPI speed, 00 is fastest (4MHz) 11 is slowest (250KHz)
-*/
+/* SPI ports are part of the hardware design. Use defaults.
+ * pin names from the pins_arduino.h file
+ * only DATAOUT and SPICLOCK are used, the rest are ignored
+ * =====+=====+========
+ * pin  | Uno | Teensy2
+ * =====+=====+========
+ * MOSI | 11  | 2
+ * MISO | 12  | 3
+ * SCLK | 13  | 1
+ * SS   | 10  | 0
+ *
+ * Data for setting the SPI control register - informational only
+ * SPCR
+ * | 7    | 6    | 5    | 4    | 3    | 2    | 1    | 0    |
+ * | SPIE | SPE  | DORD | MSTR | CPOL | CPHA | SPR1 | SPR0 |
+ * 
+ * SPIE - Enables the SPI interrupt when 1
+ * SPE  - Enables the SPI when 1
+ * DORD - Sends data least Significant Bit First when 1, most Significant Bit first
+ * MSTR - Sets the Arduino in master mode when 1, slave mode when 0
+ * CPOL - Sets the data clock to be idle when high if set to 1, idle when low if se
+ * CPHA - Samples data on the falling edge of the data clock when 1, rising edge wh
+ * SPR1 and SPR0 - Sets the SPI speed, 00 is fastest (4MHz) 11 is slowest (250KHz) */
 
 //-----
 // initialize with the length of the string
-// set height and width to a resonable number just in case one of the
-// grid functions is called
 //-----
 WS2801::WS2801(uint16_t n)
 {
+    // set height and width to a resonable number just in case one of the
+    // grid functions is called
     gridWidth=n;
     gridHeight=1;
     numLEDs = n;
 
-    pixels = (uint32_t *)malloc(numLEDs);
-    for (uint16_t i=0; i< numLEDs; i++)
-    {
-        setPixelColor(i, 0, 0, 0);
-    }
+    // allocate memory for the array and make pixels point to the start of it
+    pixels = (uint32_t *) calloc(numLEDs, sizeof(uint32_t));
+    clear();
+
+    bgpixels = (uint32_t *) calloc(numLEDs, sizeof(uint32_t));
+    bgclear();
 }
 
 
@@ -76,11 +75,12 @@ WS2801::WS2801(uint16_t x, uint16_t y)
     gridHeight=y;
     numLEDs = x * y;
 
-    pixels = (uint32_t *)malloc(numLEDs);
-    for (uint16_t i=0; i< numLEDs; i++)
-    {
-        setPixelColor(i, 0, 0, 0);
-    }
+    // allocate memory for the array and make pixels point to the start of it
+    pixels = (uint32_t *) calloc(numLEDs, sizeof(uint32_t));
+    clear();
+
+    bgpixels = (uint32_t *) calloc(numLEDs, sizeof(uint32_t));
+    bgclear();
 }
 
 
@@ -128,16 +128,32 @@ void WS2801::show(void)
     // 8 bit numbers, so bitshift and mask to get the info
     for (uint16_t p=0; p< numLEDs; p++)
     {
+      // squirt the data out to the pixel string
       // method is the same as SPI.transfer() but saves the call
-      // send red pixel
-      SPDR = pixels[p]>>16 & 0xff;
-      while (!(SPSR & (1<<SPIF))) {};
-      // send green pixel
-      SPDR = pixels[p]>>8 & 0xff;
-      while (!(SPSR & (1<<SPIF))) {};
-      // send blue pixel
-      SPDR = pixels[p] & 0xff;
-      while (!(SPSR & (1<<SPIF))) {};
+      //
+      // if the foreground is empty (0) then display the background
+      if(*(pixels+p) == 0)
+      {
+        //  *(pixels+p) = *(bgpixels+p);
+        SPDR = *(bgpixels+p)>>16 & 0xff;
+        while (!(SPSR & (1<<SPIF))) {};
+        // send green pixel
+        SPDR = *(bgpixels+p)>>8 & 0xff;
+        while (!(SPSR & (1<<SPIF))) {};
+        // send blue pixel
+        SPDR = *(bgpixels+p) & 0xff;
+        while (!(SPSR & (1<<SPIF))) {};
+      } else {
+        // send red pixel
+        SPDR = *(pixels+p)>>16 & 0xff;
+        while (!(SPSR & (1<<SPIF))) {};
+        // send green pixel
+        SPDR = *(pixels+p)>>8 & 0xff;
+        while (!(SPSR & (1<<SPIF))) {};
+        // send blue pixel
+        SPDR = *(pixels+p) & 0xff;
+        while (!(SPSR & (1<<SPIF))) {};
+      }
     }
 
     // for my protocol analyzer - see above
@@ -149,6 +165,7 @@ void WS2801::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b)
     uint32_t data;
 
     if (n > numLEDs) return;
+    if (n < 0) return;
 
     data = r;
     data <<= 8;
@@ -156,32 +173,32 @@ void WS2801::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b)
     data <<= 8;
     data |= b;
 
-    pixels[n] = data;
+    *(pixels+n) = data & 0xFFFFFF;
 }
 
 
 void WS2801::setPixelColor(uint16_t n, uint32_t c)
 {
     if (n > numLEDs) return;
+    if (n < 0) return;
 
     // mask off lower 24 bits
-    pixels[n] = c & 0xFFFFFF;
+    *(pixels+n) = c & 0xFFFFFF;
 }
-
 
 //---
 // Translate x and y to a LED index number in an array.
 //---
 uint16_t WS2801::Translate(uint16_t x, uint16_t y)
 {
+    // stop user from sending values too small or large
+    if(x<0 || x>gridWidth-1) { x=0; }
+    if(y<0 || y>gridHeight-1) { y=0; }
+
     if(x%2)
-    {
-        return(((x+1) * gridHeight)- 1 - y);
-    }
+    { return(((x+1) * gridHeight)- 1 - y); }
     else
-    {
-        return((x * gridHeight) + y);
-    }
+    { return((x * gridHeight) + y); }
 }
 
 
@@ -207,9 +224,7 @@ uint32_t WS2801::Color(uint8_t r, uint8_t g, uint8_t b)
 //=====
 void WS2801::setRange(uint16_t startLED, uint16_t endLED, uint32_t color )
 {
-    uint16_t xx;
-
-    for(xx=startLED; xx<=endLED; xx++)
+    for(int xx=startLED; xx<=endLED; xx++)
     {
         setPixelColor(startLED++, color);
     }
@@ -273,9 +288,9 @@ void WS2801::line(int x0,  int y0, int x1, int y1, uint32_t color)
     for (x=x0; x<=x1; x++)                        // from x0 to x1
     {
         if (steep) {
-            pixels[Translate(y,x)]= color;        //Fast set LEDs in grid.
+            setPixelColor(Translate(y,x), color);        //Fast set LEDs in grid.
         } else {
-            pixels[Translate(x,y)]= color;        //Fast set LEDs in grid.
+            setPixelColor(Translate(x,y), color);        //Fast set LEDs in grid.
         }
 
         error = error + deltay;
@@ -290,15 +305,87 @@ void WS2801::line(int x0,  int y0, int x1, int y1, uint32_t color)
 
 
 //=====
-// draw a box using the line drawing function (ph1x3r)
+// draw a box using the HVline drawing function (ph1x3r)
 //=====
 void WS2801::box(int x0, int y0, int x1, int y1, uint32_t color)
 {
-    line(x0,y0,x1,y0,color);
-    line(x1,y0,x1,y1,color);
-    line(x1,y1,x0,y1,color);
-    line(x0,y1,x0,y0,color);
+    HVLine(x0, y0, x1-x0, 0,color); // draw from start in right direction
+    HVLine(x1, y0, y1-y0, 1,color); // now draw downwards
+    HVLine(x1, y1, x1-x0, 2,color); // then back across to the left
+    HVLine(x0, y1, y1-y0, 3,color); // back up to the start again
 
+}
+
+//=====
+// draw a filled box using the HVline drawing function (ph1x3r)
+//=====
+void WS2801::boxFill(int x0, int y0, int x1, int y1, uint32_t color)
+{
+    // draw vertical lines the size of the box for the width
+    // of the box
+    for(int xx=x0; xx<=x1; xx++)
+    {
+      HVLine(xx, y0, y1-y0, 1, color); 
+    }
+}
+
+
+//=====
+// draw a triangle
+//=====
+void WS2801::triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color)
+{
+  line(x0, y0, x1, y1, color);
+  line(x1, y1, x2, y2, color);
+  line(x2, y2, x0, y0, color);
+}
+
+//=====
+// draw a circle
+//=====
+void WS2801::circle(uint16_t x0, uint16_t y0, uint16_t r, uint32_t color) {
+  setPixelColor(Translate(x0, y0+r), color);
+  setPixelColor(Translate(x0, y0-r), color);
+  setPixelColor(Translate(x0+r, y0), color);
+  setPixelColor(Translate(x0-r, y0), color);
+
+  circleHelp(x0, y0, r, 0xF, color);
+}
+
+void WS2801::circleHelp(uint16_t x0, uint16_t y0, uint16_t r, uint16_t corner, uint32_t color) {
+  int f = 1 - r;
+  int ddF_x = 1;
+  int ddF_y = -2 * r;
+  int x = 0;
+  int y = r;
+
+
+  while (x<y) {
+    if (f >= 0) {
+      y--;
+      ddF_y += 2;
+      f += ddF_y;
+    }
+    x++;
+    ddF_x += 2;
+    f += ddF_x;
+    if (corner & 0x4) {
+      setPixelColor(Translate(x0 + x, y0 + y), color);
+      setPixelColor(Translate(x0 + y, y0 + x), color);
+    }
+    if (corner & 0x2) {
+      setPixelColor(Translate(x0 + x, y0 - y), color);
+      setPixelColor(Translate(x0 + y, y0 - x), color);
+    }
+    if (corner & 0x8) {
+      setPixelColor(Translate(x0 - y, y0 + x), color);
+      setPixelColor(Translate(x0 - x, y0 + y), color);
+    }
+    if (corner & 0x1) {
+      setPixelColor(Translate(x0 - y, y0 - x), color);
+      setPixelColor(Translate(x0 - x, y0 - y), color);
+    }
+  }
 }
 
 
@@ -316,7 +403,7 @@ void WS2801::comet(int wait, int red, int green, int blue, int tail)
         xx=tail;
         for(yy=254; yy>=0; yy=yy-(abs(255/tail)))
         {
-            setPixelColor(zz-xx,
+            setPixelColor(constrain(zz-xx, 0, numLEDs-1),
                 Color(
                 constrain(red-yy, 0, 254),
                 constrain(green-yy, 0, 254),
@@ -336,20 +423,19 @@ void WS2801::comet(int wait, int red, int green, int blue, int tail)
 //=====
 void WS2801::AllRandom()
 {
-    int xx;
-    for(xx=0;xx<numLEDs;xx++)
+    for(int xx=0; xx<numLEDs; xx++)
     {
-        setPixelColor(xx, Color(random(0,254),random(0,254),random(0,254) ) );
+      setPixelColor(xx, random(0, 0xffffff) );
     }
 }
 
 
 //=====
-//Set all LEDS to a single color.
+//Set all LEDS to a single color. (ph1x3r)
 //=====
 void WS2801::All( uint32_t color)
 {
-    setRange(0,numLEDs-1,color);
+    setRange(0, numLEDs-1, color);
 }
 
 
@@ -361,8 +447,7 @@ void WS2801::All( uint32_t color)
 //=====
 void WS2801::Twinkle(int wait, int repeat)
 {
-    int x;
-    for(x=0;x<repeat;x++)
+    for(int xx=0; xx<repeat; xx++)
     {
         AllRandom();
         show();
@@ -377,16 +462,16 @@ void WS2801::Twinkle(int wait, int repeat)
 void WS2801::MoveLinesH(int del, uint32_t col)
 {
     int xx;
-    for(xx=0;xx<gridHeight;xx++)
+    for(xx=0; xx<gridHeight; xx++)
     {
-        line(0,xx,gridWidth-1,xx,col );
+        line(0, xx, gridWidth-1, xx, col );
         show();
         delay(del);
     }
 
-    for(xx=0;xx<gridHeight;xx++)
+    for(xx=0; xx<gridHeight; xx++)
     {
-        line(0,xx,gridWidth-1,xx,0 );
+        line(0, xx, gridWidth-1, xx, 0 );
         show();
         delay(del);
     }
@@ -399,16 +484,16 @@ void WS2801::MoveLinesH(int del, uint32_t col)
 void WS2801::MoveLinesRH(int del, uint32_t col)
 {
     int xx;
-    for(xx=gridHeight;xx>-1;xx--)
+    for(xx=gridHeight-1; xx>=0; xx--)
     {
-        line(0,xx,gridWidth-1,xx,col );
+        line(0, xx, gridWidth-1, xx, col );
         show();
         delay(del);
     }
 
-    for(xx=gridHeight;xx>-1;xx--)
+    for(xx=gridHeight-1; xx>=0; xx--)
     {
-        line(0,xx,gridWidth-1,xx,0 );
+        line(0, xx, gridWidth-1, xx, 0 );
         show();
         delay(del);
     }
@@ -421,16 +506,16 @@ void WS2801::MoveLinesRH(int del, uint32_t col)
 void WS2801::MoveLinesV(int del, uint32_t col)
 {
     int xx;
-    for(xx=0;xx<gridWidth;xx++)
+    for(xx=0; xx<gridWidth; xx++)
     {
-        line(xx,0,xx,gridHeight-1,col );
+        line(xx, 0, xx, gridHeight-1, col );
         show();
         delay(del);
     }
 
-    for(xx=0;xx<gridWidth;xx++)
+    for(xx=0; xx<gridWidth; xx++)
     {
-        line(xx,0,xx,gridHeight-1,0 );
+        line(xx, 0, xx, gridHeight-1, 0 );
         show();
         delay(del);
     }
@@ -443,16 +528,16 @@ void WS2801::MoveLinesV(int del, uint32_t col)
 void WS2801::MoveLinesRV(int del, uint32_t col)
 {
     int xx;
-    for(xx=gridWidth;xx>-1;xx--)
+    for(xx=gridWidth-1; xx>=0; xx--)
     {
-        line(xx,0,xx,gridHeight-1,col );
+        line(xx, 0, xx, gridHeight-1, col );
         show();
         delay(del);
     }
 
-    for(xx=gridWidth;xx>-1;xx--)
+    for(xx=gridWidth-1; xx>=0; xx--)
     {
-        line(xx,0,xx,gridHeight-1,0 );
+        line(xx, 0, xx, gridHeight-1, 0 );
         show();
         delay(del);
     }
@@ -487,13 +572,32 @@ void WS2801::SpinLine(int del, uint32_t col)
     }
 }
 
-
 //=====
 // shortcut for erasing all of the pixels at once (ph1x3r)
 //=====
 void WS2801::clear()
 {
-    for (uint16_t i=0; i< numLEDs; i++) {
-        pixels[i] = 0;
+    for(int i=0; i< numLEDs; i++)
+    {
+        *(pixels+i) = 0;
+    }
+}
+
+//=====
+// functions for working with the background (ph1x3r)
+//=====
+void WS2801::bgclear()
+{
+    for(int i=0; i< numLEDs; i++)
+    {
+        *(bgpixels+i) = 0;
+    }
+}
+
+void WS2801::bgAll(uint32_t col)
+{
+    for(int xx=0;xx<numLEDs;xx++)
+    {
+      *(bgpixels+xx) = col & 0xffffff;
     }
 }
